45c51cc39ba1ba5fc66b70dfc0f93998363d21d3
utkuali <utkuali11@hotmail.com>
2019-12-18 12:23:48 +0300

    forgot to add Config.selltime

--- config.lua
+++ config.lua
@@ -4,6 +4,7 @@ Config.Locale = "en"
 
 Config.rndfail = math.random(1, 500) -- Percantage of purchase fail = 1/500 (money always removed)
 Config.deltime = 900000 -- 15min
+Config.selltime = 900000
 Config.enablenpc = true -- NPC enemies during sell
 
 Config.Locations = {
